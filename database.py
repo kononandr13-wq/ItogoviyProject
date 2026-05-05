8a3638ff9895489a6048ea529513b06bc69a195c
A1exSid <alexsiboleg@gmail.com>
2026-05-05 10:45:03 +0700

    .
    database//////

--- database.py
+++ database.py
@@ -2,16 +2,21 @@ import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 
 def create_db():
-    conn = sqlite3.connect("todo.db")
+    conn = sqlite3.connect("fcb.db")
     cursor = conn.cursor()
 
     sql = '''
         CREATE TABLE IF NOT EXISTS Dostyp (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            level VARCHAR(1000) NOT NULL,
+            level VARCHAR(1000) NOT NULL
         )
     '''
     cursor.execute(sql)
+
+    # Добавление 3 уровней доступа (Первый, Второй, Третий)
+    cursor.execute("INSERT INTO Dostyp(level) VALUES('Первый уровень')")
+    cursor.execute("INSERT INTO Dostyp(level) VALUES('Второй уровень')")
+    cursor.execute("INSERT INTO Dostyp(level) VALUES('Третий уровень')")
 
     sql = '''
         CREATE TABLE IF NOT EXISTS gos_persona (
@@ -27,7 +32,7 @@ def create_db():
         )
     '''
     cursor.execute(sql)
-    conn.commit()
+    
 
     sql = '''
         CREATE TABLE IF NOT EXISTS file (
@@ -35,7 +40,7 @@ def create_db():
             reason VARCHAR(1000) NOT NULL,
             notes VARCHAR(1000) NOT NULL,
             photo VARCHAR(1000) NOT NULL,
-            tickets VARCHAR(1000) NOT NULL,
+            tickets VARCHAR(1000) NOT NULL
         )
     '''
     cursor.execute(sql)
@@ -53,20 +58,20 @@ def create_db():
     '''
     cursor.execute(sql)
 
-
+    conn.commit()
 
     conn.close()
 
 def get_users():
-    conn = sqlite3.connect("todo.db")
+    conn = sqlite3.connect("fcb.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM User")
+    cursor.execute("SELECT * FROM Dostyp")
     users = cursor.fetchall()
     conn.close()
     return users
 
 def auth_user(login, password):
-    conn = sqlite3.connect("todo.db")
+    conn = sqlite3.connect("fcb.db")
     cursor = conn.cursor()
 
     cursor.execute(
@@ -84,20 +89,27 @@ def auth_user(login, password):
     else:
         return -1
 
-def add_user(login, password):
-    conn = sqlite3.connect("todo.db")
+def add_citizens(name,surname, patronymic,is_wanted):
+    conn = sqlite3.connect("fcb.db")
     cursor = conn.cursor()
-    hashed_password = generate_password_hash(password)
+
     cursor.execute(
-        "INSERT INTO User (login, password) VALUES (?,?)",
-        (login, hashed_password)
+        "INSERT INTO file(reason,notes,photo,tickets)VALUES(?,?,?,?)",
+        ('', '','','')
     )
-    print("Создан пользователь " + login)
+
+    new_file_id = cursor.lastrowid
+
+    cursor.execute(
+        "INSERT INTO Citizens (name,surname, patronymic,is_wanted,file_id) VALUES (?,?,?,?,?)",
+        (name,surname, patronymic,is_wanted, new_file_id)
+    )
+    print("Создан пользователь " + name,surname,patronymic)
     conn.commit()
     conn.close()
 
 def is_user_exists(login):
-    conn = sqlite3.connect("todo.db")
+    conn = sqlite3.connect("fcb.db")
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM User WHERE login = ?", (login,))
     user = cursor.fetchone()
@@ -106,9 +118,8 @@ def is_user_exists(login):
 
 if __name__ == "__main__":
     create_db()
-    add_user("21", "21")
-    add_user("user", "user")
-    users = get_users()
+    add_citizens("tIMUR", "sHUPOAWLDKAWLJKD", "HZ", 1)
+users = get_users()
 
 
 
